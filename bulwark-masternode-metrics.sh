e0f7de2f058f3d21f1f9356f23b4cf32dffc95cb
u74r0 <u74r0crypto@gmail.com>
2018-05-14 10:25:03 +0900

    Do not use temporary block info file and improve logic

--- bulwark-masternode-metrics.sh
+++ bulwark-masternode-metrics.sh
@@ -3,8 +3,9 @@
 # Output Bulwark masernode metrics data for Mackerel
 
 bulwarkcli="/usr/local/bin/bulwark-cli -conf=/root/.bulwark/bulwark.conf"
-block_info_1day_ago_path="/tmp/bulwark-masternode-metrics-block-info-1day-ago.tmp"
 
+# Number of blocks generated average in 24 hours
+block_generated_size_day=912
 current_time=`date +%s`
 
 #
@@ -17,31 +18,33 @@ print_metric () {
   echo "bulwark.$1\t$2\t${current_time}"
 }
 
+# Print block elapsed time since block generated
+#
+# @param string $1 Block height
+get_block_elapsed_time () {
+  block_hash=`${bulwarkcli} getblockhash $1`
+  block_time=`${bulwarkcli} getblock $block_hash | jq .time`
+  elapsed_time=$((current_time - block_time))
+  echo $elapsed_time
+}
+
+# Get generated block height in 24 hours
 block_count=`${bulwarkcli} getblockcount`
-
-# Get over 24 hours block height
-block_height=$((block_count - 1000))
-if [ -e $block_info_1day_ago_path ]; then
-  block_info_1day_ago_height=`cat $block_info_1day_ago_path | jq .height`
-  if [ "$block_info_1day_ago_height" -gt 0 ]; then
-    block_height="$block_info_1day_ago_height"
-  fi
-fi
-
-while :; do
-  block_hash=`${bulwarkcli} getblockhash $block_height`
-  block=`${bulwarkcli} getblock $block_hash`
-  block_time=`echo $block | jq .time`
-  block_info_elapsed=$((current_time - block_time))
-
-  if [ 86400 -ge $block_info_elapsed ]; then 
-    echo $block > $block_info_1day_ago_path
-    break;
-  fi 
-
+block_height=$((block_count - block_generated_size_day))
+block_generated_elapsed=`get_block_elapsed_time $block_height`
+if [ $block_generated_elapsed -gt 86400 ]; then
+  while [ $block_generated_elapsed -gt 86400 ]; do
+    block_height=$((block_height + 1))
+    block_generated_elapsed=`get_block_elapsed_time $block_height`
+  done
+else
+  while [ 86400 -ge $block_generated_elapsed ]; do
+    block_height=$((block_height - 1))
+    block_generated_elapsed=`get_block_elapsed_time $block_height`
+  done
   block_height=$((block_height + 1))
-done
-block_count_day=$((block_count - block_height))
+fi
+block_generated_day=$((block_count - block_height))
 
 # Get master node address on this server
 addr=`${bulwarkcli} masternode status | jq -r .addr`
@@ -70,7 +73,7 @@ metric_value=`echo "scale=3; $activetime / 86400" | bc`
 print_metric activetime.days $metric_value
 
 # Output estimate payout hours
-estimate_payout_hours=`echo "scale=2; 24 / ($block_count_day / $masternode_stable)" | bc`
+estimate_payout_hours=`echo "scale=2; 24 / ($block_generated_day / $masternode_stable)" | bc`
 print_metric payout.estimate_payout $estimate_payout_hours
 
 # Output last paid elapsed time
